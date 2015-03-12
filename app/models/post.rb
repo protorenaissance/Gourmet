065155ea6f30a4a63f07bb0f0b1275d6a35528fa
Codelion <Protorenaissance@gmail.com>
2015-03-12 12:30:31 +0000

    Carrierwave gem preparation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	has_many:comments
 	validates:category,:inclusion=>{:in=>["Korean","Japanese","Chinese","Western"],:message=>"Pick one between Korean, Chinese, Japanese, Western"}
 	validates:title,:presence=>{:message=>"You should input a title"}
+	mount_uploader:image,ImageUploader
 end
